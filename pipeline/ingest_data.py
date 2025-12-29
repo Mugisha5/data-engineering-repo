935c645ade5ca95f9594743ce0d92ac32d8fed45
Christin M <mugishachristin@gmail.com>
2025-12-29 16:14:19 -0500

    First Part Docker For dataEng completed

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,6 +5,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 year = 2021
 month = 1
@@ -35,33 +36,37 @@ parse_dates = [
 prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
 url = f'{prefix}//yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-df = pd.read_csv(url, dtype=dtype, parse_dates=parse_dates)
 
 
 
 
-def run():
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='Postgres user')
+@click.option('--pg-password', default='root', show_default=False, help='Postgres password')
+@click.option('--pg-host', default='localhost', show_default=True, help='Postgres host')
+@click.option('--pg-port', default='5432', show_default=True, help='Postgres port')
+@click.option('--pg-db', default='ny_taxi', show_default=True, help='Postgres database name')
+@click.option('--chunk-size', default=100000, type=int, show_default=True, help='CSV chunk size')
+@click.option('--table-name', default='yellow_taxi_data', show_default=True, help='Destination table name')
+@click.option('--year', default=2021, type=int, show_default=True, help='Year of the dataset')
+@click.option('--month', default=1, type=int, show_default=True, help='Month of the dataset')
+def run(pg_user, pg_password, pg_host, pg_port, pg_db, chunk_size, table_name, year, month):
 
-    pg_user =  'root'
-    pg_password = 'root'    
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    chunk_size = 100000
-    table_name = 'yellow_taxi_data'
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{prefix}//yellow_tripdata_{year}-{month:02d}.csv.gz'
+
+    df = pd.read_csv(url, dtype=dtype, parse_dates=parse_dates)
+
     engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
 
-    df.head(0).to_sql(name="yellow_taxi_data", con= engine,if_exists='replace')
+    df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    
-    df_iter = pd.read_csv(url, dtype=dtype, parse_dates=parse_dates,iterator=True, chunksize=chunk_size)
-
+    df_iter = pd.read_csv(url, dtype=dtype, parse_dates=parse_dates, iterator=True, chunksize=chunk_size)
 
     for df_chunk in tqdm(df_iter):
         df_chunk.to_sql(
-
-            name=table_name, 
-            con= engine,
+            name=table_name,
+            con=engine,
             if_exists='append'
         )
 
